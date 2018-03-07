3e8d312d87a0cbc865e9a4471a56c235c81b876b
Egor Piskunou <egor.piskunou@gmail.com>
2018-03-07 17:49:42 +0300

    declare routes

---
+++ index.js
@@ -0,0 +1,95 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const handlers = {
+  '/api/articles/readall': readall,
+  '/api/articles/read': read,
+  '/api/articles/create': createArticle,
+  '/api/articles/update': updateArticle,
+  '/api/articles/delete': removeArticle,
+  '/api/comments/create': createComment,
+  '/api/comments/delete': removeComment
+};
+
+const server = http.createServer((req, res) => {
+  const handler = getHandler(req.url);
+
+  handler(req, res, (err, result) => {
+    if (err) {
+      res.statusCode = err.code;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(err));
+      return;
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function getHandler(url) {
+  return handlers[url] || notFound;
+}
+
+function readall(req, res, cb) {
+  fs.readFile('./articles.json', (err, data) => {
+    if(err) console.error('[ERROR] Some error in read file')
+    cb(null, JSON.parse(data));
+  });
+}
+
+function read(req, res, cb) {
+  fs.readFile('./article.json', (err, data) => {
+    if(err) console.error('[ERROR] Some error in read file')
+    const articles = JSON.parse(data);
+    cb(null, articles.map(article => {
+      if(article.id === req.id) return
+    }));
+  });
+}
+
+function createArticle() {
+
+}
+
+function updateArticle() {
+  
+}
+
+function removeArticle() {
+  
+}
+
+function createComment() {
+  
+}
+
+function removeComment() {
+  
+}
+
+function notFound(req, res, cb) {
+  cb({ code: 404, message: 'Not found'});
+}
+
+function parseBodyJson(req, cb) {
+  let body = [];
+
+  req.on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+
+    let params = JSON.parse(body);
+
+    cb(null, params);
+  });
+}
